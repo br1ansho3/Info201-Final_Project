d0a436bf20effdacb62409f05100aa62f329c8ec
br1ansho3 <brianhsu860906@gmail.com>
2019-02-28 15:20:51 -0800

    test github slack

--- api-keys.R
+++ api-keys.R
@@ -1,5 +1,5 @@
 nutrition-key <- "kv8BbI66qy7KLhPW9t4Q51bsJ4Ju4nUCpigwKGRn"
 
-Recipe_key <- "3feeabd627msh661da213da59680p1bff94jsn9f2df977c019"
+recipe_key <- "3feeabd627msh661da213da59680p1bff94jsn9f2df977c019"
 
-Recipe_url <- "https://spoonacular.com/food-api"
+recipe_url <- "https://spoonacular.com/food-api"
