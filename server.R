b1ca9e9161c1ec87e76e61ea6ab356dbb7e34884
br1ansho3 <brianhsu860906@gmail.com>
2019-03-09 17:53:20 -0800

    complete add ingredient feature (button)

--- server.R
+++ server.R
@@ -1,5 +1,27 @@
 library(shiny)
+library(shinyjs)
+
+source("api-keys.R")
+base_uri <- "https://webknox-recipes.p.rapidapi.com/recipes/"
+query_param <- list(X-RapidAPI-Key = reci)
+
 
 server <- function(input, output) {
+  ingredients_list <- tagList()
   
+  output$all_ingredients <- renderUI({
+    counter <- input$add_ingredient
+    print(counter)
+    if(is.null(counter) | counter < 1) return()
+    id <- paste0("ingredient_", counter)
+    label <- paste0("Ingredient ", counter, ":")
+    widget <- textInput(id, label)
+    ingredients_list <<- tagAppendChild(ingredients_list, widget)
+    if(counter == 5) disable("add_ingredient")
+    ingredients_list
+  })
+  
+  url <- reactive({
+    response <- GET
+  })
 }
--- ui.R
+++ ui.R
@@ -1,16 +1,46 @@
 library(shiny)
+library(httr)
+library(jsonlite)
+library(shinyjs)
+# variables
+intolerace <- c("dairy", "gluten", "caffeine")
+
+
+#---------------
 
 home_page <- tabPanel("About Us")
 first_page <- tabPanel("Recipes given constraints")
 second_page <- tabPanel("Nutrition of Food")
-third_page <- tabPanel("Recipe from Ingredients")
+third_page <- tabPanel(
+  "Recipe from Ingredients",
+  titlePanel("What can I make?"),
+  sidebarLayout(
+    sidebarPanel(
+      #textInput(inputId = "ingredient_1", label = "", placeholder = "Ingredient 1"),
+      #textInput(inputId = "ingredient_2", label = "", placeholder = "Ingredient 2"),
+      #textInput(inputId = "ingredient_3", label = "", placeholder = "Ingredient 3"),
+      uiOutput("all_ingredients"),
+      actionButton("add_ingredient", "Add Ingredient"),
+      hr(),
+      h1("Filters:"),
+      selectInput(inputId = "intolerace", label = "Intolerance", choices = intolerace)
+      
+    ),
+    mainPanel(
+      
+    )
+  )
+)
 
 
 
-ui <- navbarPage(
-  "Recipeeeez",
-  home_page,
-  first_page,
-  second_page,
-  third_page
+ui <- tagList(
+  useShinyjs(),
+  navbarPage(
+    "Recipeeeez",
+    home_page,
+    first_page,
+    second_page,
+    third_page
+  )
 )
