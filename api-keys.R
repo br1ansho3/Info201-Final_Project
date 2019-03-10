cf25d348b9af7be7ed5163a01a4b6bdd91907594
br1ansho3 <brianhsu860906@gmail.com>
2019-03-09 17:54:12 -0800

    fix api_key variable

--- api-keys.R
+++ api-keys.R
@@ -1,4 +1,4 @@
-nutrition-key <- "kv8BbI66qy7KLhPW9t4Q51bsJ4Ju4nUCpigwKGRn"
+nutrition_key <- "kv8BbI66qy7KLhPW9t4Q51bsJ4Ju4nUCpigwKGRn"
 
 recipe_key <- "3feeabd627msh661da213da59680p1bff94jsn9f2df977c019"
 
