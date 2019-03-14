522d757039849e154ad609eb6a28317d536d653a
YanRuiqi <Rachel@D-10-19-142-180.dhcp4.washington.edu>
2019-03-13 19:51:58 -0700

    change style

--- ui.R
+++ ui.R
@@ -92,12 +92,12 @@ third_page <- tabPanel(
 
 ui <- tagList(
   navbarPage(
-    useShinyjs(),
     title = "RecipEZ",
     home_page,
     first_page,
     second_page,
     third_page,
-    theme = "style.css"
+    theme = "style.css",
+    useShinyjs()
   )
 )
